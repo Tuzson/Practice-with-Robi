e9cc28b31008cfc26e61ca5c978bfb38d18fd711
Tuzson <kecseyt@gmail.com>
2020-11-10 14:47:24 +0100

    Fields added + Constructor + Getters/Setters

---
+++ src/com/company/Person.java
@@ -0,0 +1,127 @@
+package com.company;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String address;
+    private String city;
+    private String country;
+    private String state;
+    private int zip;
+    private String phone1;
+    private String phone2;
+    private String email;
+    private String web;
+
+    public Person(String firstName, String lastName, String companyName, String address, String city, String country, String state, int zip, String phone1, String phone2, String email, String web) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.companyName = companyName;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.state = state;
+        this.zip = zip;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+        this.web = web;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+}
